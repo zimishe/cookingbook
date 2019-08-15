9edf7aa7dc6eb8dd619d87bd186cd9579a99a05b
Yevhen Shuliakivskiy <zimishe@gmail.com>
2019-08-15 21:02:23 +0300

    implemented microservce to check for token

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const { URL } = require('./_config/db');
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -9,21 +7,13 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.setHeader('Access-Control-Allow-Methods', 'POST');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
 
-MongoClient.connect(URL,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	},
-	(err, database) => {
-		if (err) return console.log(err);
-		require('./routes')(app, database);
+require('./routes')(app);
 
-		app.listen(port, () => {
-			console.log('Server is live at ', port);
-		});
+app.listen(port, () => {
+	console.log('Server is live at ', port);
 });
--- src/routes/authToken.js
+++ src/routes/authToken.js
@@ -1,19 +1,5 @@
-const uuidv1 = require('uuid/v1');
-const { dbName } = require('../_config/db');
-
 module.exports = (app, client) => {
-	app.post('/recipes', (req, res) => {
-		const dateAdded = new Date().toISOString();
-		const recipe = { ...req.body, dateAdded, id: uuidv1() };
-
-		const db = client.db(dbName);
-
-		db.collection('recipes').insert(recipe, (error, result) => {
-			if (error) {
-				res.send({ error });
-			} else {
-				res.send(result.ops[0]);
-			}
-		});
+	app.post('/auth', ({ body = {} }, res) => {
+		res.sendStatus(Boolean(body.token) ? 200 : 401)
 	})
 }
